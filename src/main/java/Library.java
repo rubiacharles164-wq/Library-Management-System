d208736480d8ff701781b6b0cf56ad8521e1b6e7
Charles <rubiacharles164@gmail.com>
2026-06-05 21:22:53 -0230

    added an update with error, and a search by with code and error for code and series

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Library {
     static List<Books> library = new ArrayList<>();
@@ -28,12 +29,27 @@ public class Library {
         }
     }
 
-    public static class findBookBySeries {
-        public findBookBySeries(String findBook) {
+    public static class findBookByCode{
+        public findBookByCode(String findBook) throws IOException{
 
             for (int i = 0; i < library.size(); i++){
-                if (findBook.equals(library.get(i).getSeries()))
-                 System.out.println(library.get(i).toString());
+                if (!findBook.equals(library.get(i).getCode()))
+                    throw new IOException("Code does not match a book");
+                else {
+                    System.out.println(library.get(i).toString());
+                }
+            }
+        }
+    }
+
+    public static class findBookBySeries {
+        public findBookBySeries(String findBook) throws IOException{
+            for (int i = 0; i < library.size(); i++){
+                if (!findBook.equals(library.get(i).getSeries()))
+                    throw new IOException("Series does not match a book");
+                else {
+                    System.out.println(library.get(i).toString());
+                }
             }
         }
     }
@@ -59,6 +75,20 @@ public class Library {
                 else {
                     library.remove(deleteBook);
                     System.out.println(deleteBook + " Has been deleted from library Data");
+                }
+            }
+        }
+    }
+
+
+    public static class updateByID {
+        public updateByID(Object update, Books updateNew) throws IOException{
+            for (int i = 0; i < library.size(); i++) {
+                if (!update.equals(library.get(i).getCode()))
+                    throw new IOException("ID does not match a code in the Library");
+                else {
+                    library.remove(update);
+                    library.add(updateNew);
                 }
             }
         }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class LibraryTest{
 
@@ -21,9 +22,22 @@ public class LibraryTest{
         new Library.displayAllBooks();
     }
 
+    @Test //works But Throws IOExceoptions
+    public void TestSearchByCode() throws IOException {
+        new Library.findBookByCode("BO2");
+    }
     @Test
-    public void TestSearchBySeries() {
+    public void TestSearchByCodeWithError() throws IOException {
+        new Library.findBookByCode("BO6");
+    }
+
+    @Test   //works But Throws IOExceoptions
+    public void TestSearchBySeries() throws IOException {
         new Library.findBookBySeries("Percy Jackson");
+    }
+    @Test
+    public void TestSearchBySeriesWithError() throws IOException {
+        new Library.findBookBySeries("Diary of a Wimpy Kid");
     }
 
     @Test
@@ -43,5 +57,15 @@ public class LibraryTest{
         new Library.deleteBook(deleteBook);
     }
 
-
+    @Test //doesnt work, not sure how to check specifically just one part then alter the rest of one class
+    public void TestupdateBook() throws IOException {
+        Books updateNew = new Books("BO1", "Lord of The Rings", "The Two Towers", "November, 11 1954");
+        new Library.updateByID("BO1", updateNew);
+    }
+    @Test
+    public void TestupdateBookWithError() throws IOException {
+        Object update = "BO4";
+        Books updateNew = new Books("BO4", "Lord of The Rings", "The Two Towers", "November, 11 1954");
+        new Library.updateByID(update, updateNew);
+    }
 }
